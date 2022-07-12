61d25f13d05cf954c9691a9497ee2142c3d7be79
Aaron Jones <ernypoo92@gmail.com>
2022-07-12 16:50:04 -0600

    created and styled about page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Name from './Components/Name'
+import About from './Components/About'
 
 function App() {
   return (
     <div className="App font-Body text-2xl">
       <Name />
+      <About />
     </div>
   );
 }
---
+++ src/Components/About.js
@@ -0,0 +1,14 @@
+const About = () => {
+    return ( 
+        <div className="about bg-white-dark">
+            <div className="p-28 text-left">
+                <h3 className="font-Tech text-3xl border-b-2">About</h3>
+                <h2 className="font-bold tracking-wider text-4xl antialiased">I am a bright-eyed full stack developer</h2>
+                <p className="pt-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, natus asperiores! Ut libero laboriosam porro temporibus rerum sapiente dolorum aliquid tenetur quibusdam aperiam? Laborum sunt rerum cupiditate atque minima hic!</p>
+            </div>
+            
+        </div>
+    );
+}
+ 
+export default About;
