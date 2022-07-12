a76bcf881ccf099bb5a1381c9f66b58d5c2a125b
Aaron Jones <ernypoo92@gmail.com>
2022-07-12 10:46:59 -0600

    first commit

---
+++ src/Components/Name.js
@@ -0,0 +1,19 @@
+const Name = () => {
+    return ( 
+        <div className="name">
+            <h3>Hi, my name is</h3>
+            <h1>Aaron Jones.</h1>
+            <h3>I am a full stack developer</h3>
+            <div>
+                <a href="">
+                    <img src="" alt="Link to LinkedIn Profile" />
+                </a>
+                <a href="">
+                    <img src="" alt="Link to Github Profile" />
+                </a>
+            </div>
+        </div>
+    );
+}
+ 
+export default Name;
